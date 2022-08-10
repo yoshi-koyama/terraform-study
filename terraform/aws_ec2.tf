3344d1884aa4e3e23b42f5560f894b9f75b7b9e9
小山由人 <koyama@reytech.co.jp>
2022-08-10 19:33:10 +0900

    add resources

--- terraform/aws_ec2.tf
+++ terraform/aws_ec2.tf
@@ -1,15 +1,15 @@
-# #----------------------------------------
-# # EC2インスタンスの作成
-# #----------------------------------------
-# resource "aws_instance" "sample_web_server" {
-#   ami                    = "ami-09d28faae2e9e7138" # Amazon Linux 2
-#   instance_type          = "t2.micro"
-#   subnet_id              = aws_subnet.sample_subnet.id
-#   vpc_security_group_ids = [aws_security_group.sample_sg.id]
-#   user_data              = <<EOF
-# #! /bin/bash
-# sudo yum install -y httpd
-# sudo systemctl start httpd
-# sudo systemctl enable httpd
-# EOF
-# }
+#----------------------------------------
+# EC2インスタンスの作成
+#----------------------------------------
+resource "aws_instance" "sample_web_server" {
+  ami                    = "ami-09d28faae2e9e7138" # Amazon Linux 2
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.sample_subnet.id
+  vpc_security_group_ids = [aws_security_group.sample_sg.id]
+  user_data              = <<EOF
+#! /bin/bash
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+EOF
+}
