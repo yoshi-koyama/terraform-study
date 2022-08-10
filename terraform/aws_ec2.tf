9dc1ae0ef65079717d7b7333401857c5465bb232
小山由人 <koyama@reytech.co.jp>
2022-08-10 20:11:51 +0900

    up ec2

--- terraform/aws_ec2.tf
+++ terraform/aws_ec2.tf
@@ -1,15 +1,15 @@
-# #----------------------------------------
-# # EC2インスタンスの作成
-# #----------------------------------------
-# resource "aws_instance" "sample_web_server" {
-#   ami                    = "ami-09d28faae2e9e7138" # Amazon Linux 2
-#   instance_type          = "t2.micro"
-#   subnet_id              = aws_subnet.sample_subnet.id
-#   vpc_security_group_ids = [aws_security_group.sample_sg.id]
-#   user_data              = <<EOF
-# #! /bin/bash
-# sudo yum install -y nginx
-# sudo systemctl start nginx
-# sudo systemctl enable nginx
-# EOF
-# }
+#----------------------------------------
+# EC2インスタンスの作成
+#----------------------------------------
+resource "aws_instance" "sample_web_server" {
+  ami                    = "ami-09d28faae2e9e7138" # Amazon Linux 2
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.sample_subnet.id
+  vpc_security_group_ids = [aws_security_group.sample_sg.id]
+  user_data              = <<EOF
+#! /bin/bash
+sudo yum install -y nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+EOF
+}
