f9df1c98fbcbea58fb37e5ea56ef8baed64c58be
小山由人 <koyama@reytech.co.jp>
2022-08-10 19:29:33 +0900

    remove s3 bucket definition

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,9 +25,9 @@ provider "aws" {
   }
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-raisetech-study-tfstate"
-  versioning {
-    enabled = true
-  }
-}
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "terraform-raisetech-study-tfstate"
+#   versioning {
+#     enabled = true
+#   }
+# }
